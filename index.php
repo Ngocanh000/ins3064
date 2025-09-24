4c2708b604dd406ba2c726c4ca0f6a9b740df6c7
Ngocanh000 <22071009@vnu.edu.vn>
2025-09-24 13:38:52 +0700

    First commit - add index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>INS3064 Web</title>
+</head>
+<body>
+  <h1>Xin chào, đây là web đầu tiên!</h1>
+  <p>Nếu bạn thấy trang này thì server đã chạy thành công.</p>
+</body>
+</html>
