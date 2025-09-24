4a8e65854e3f5409bfd63a17a76c6988b8c0c99f
Ngocanh000 <22071009@vnu.edu.vn>
2025-09-24 15:12:20 +0700

    Create lesson2.php
    up lesson

---
+++ lesson2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <?php
+    $name = "Jade Nelson";
+    $age = 20;
+    $courses = array("Java", "C", "PHP");
+    echo "Name: " . $name . ", age: ".$age. 
+    "<br/>3rd course is: " . $courses[1];
+    ?>
+</body>
+</html> 
